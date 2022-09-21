79dc8d5b7cc1225cb294e385e847187793a4a1da
Septio Nugroho <septio.nugroho@ui.ac.id>
2022-09-21 22:14:34 +0700

    menambah testing

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from multiprocessing.connection import Client
+from urllib import response
 
 # Create your tests here.
+class MywatchlistTest(TestCase):
+    def test_request_html(self):
+
+        response = self.client.get('/mywatchlist/html/')
+        self.assertEqual(response.status_code,200)
+
+    def test_request_xml(self):
+
+        response = self.client.get('/mywatchlist/xml/')
+        self.assertEqual(response.status_code,200)
+
+    def test_request_json(self):
+
+        response = self.client.get('/mywatchlist/json/')
+        self.assertEqual(response.status_code,200)
--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -8,7 +8,7 @@ from mywatchlist.views import show_mywatchlist_json_by_id
 app_name = 'mywatchlist'
 
 urlpatterns = [
-    path('', show_mywatchlist, name='show_mywatchlist'),
+    path('html/', show_mywatchlist, name='show_mywatchlist'),
     path('xml/', show_mywatchlist_xml, name='show_mywatchlist_xml'),
     path('json/', show_mywatchlist_json, name='show_mywatchlist_json'),
     path('json/<int:id>', show_mywatchlist_json_by_id, name='show_json_by_id'),
